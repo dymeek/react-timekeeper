d4b58b744d5abf19bd3c043e5fc6da486ea76264
Paweł Dymon <dymonpawel@gmail.com>
2022-10-28 17:41:00 +0200

    Updates

---
+++ precache-manifest.20519170851efc3c159d6a46d070bd1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e762365064687336f403",
+    "url": "/react-timekeeper/static/css/main.cad5918b.chunk.css"
+  },
+  {
+    "revision": "e762365064687336f403",
+    "url": "/react-timekeeper/static/js/main.9a94b95b.chunk.js"
+  },
+  {
+    "revision": "ee0f81cea3179d0574ea",
+    "url": "/react-timekeeper/static/js/runtime~main.fdf0ed0a.js"
+  },
+  {
+    "revision": "7c0ba17f1dece9f6aa68",
+    "url": "/react-timekeeper/static/js/2.2626944e.chunk.js"
+  },
+  {
+    "revision": "913881ce6dfbc31ab6e5c7517e20fc2a",
+    "url": "/react-timekeeper/index.html"
+  }
+];
